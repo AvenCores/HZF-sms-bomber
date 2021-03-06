5929e75012663a1f90ee2322160dbb40c5c86809
AvenCores <64781822+AvenCores@users.noreply.github.com>
2021-03-06 14:46:51 +0300

    Update Install.py

--- Install.py
+++ Install.py
@@ -2,7 +2,6 @@ import os
 
 print("Установка библиотек")
 os.system("pip3 install --upgrade pip")
-print("")
 bib = ["tk", "colorama", "bs4", "as", "termcolor", "Label", "colored", "requests"]
 for i in range(len(bib)):
     print("Установка "+bib[i]+"...\033[0m")
