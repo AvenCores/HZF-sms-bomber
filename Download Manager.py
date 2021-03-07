73ac78c43d9d9258d3f56bcff8320845ba986681
AvenCores <64781822+AvenCores@users.noreply.github.com>
2021-03-07 15:16:53 +0300

    Update Download Manager.py

--- Download Manager.py
+++ Download Manager.py
@@ -4,23 +4,36 @@ from os import mkdir
 import requests
 
 root = Tk()
-root.title('HZF Bomber Downloader')
-root.geometry('350x120')
+root.title('Download Manager')
+root.geometry('350x140')
 root.resizable(width=False, height=False)
 
-def download():
+def downloadSms():
         mkdir("c://Bomber")
         f=open(r'c:/Bomber/HZF Bomber.zip',"wb")
         ufr = requests.get("https://github.com/AvenCores/HZF-sms-bomber/releases/download/V1.2/HZF.SMS.BOMBER.V1.2.zip")
         f.write(ufr.content)
         f.close()
-        messagebox.showinfo(title="Удачно", message='Бомбер был скачен в папку C:\Bomber')
+        messagebox.showinfo(title="Удачно", message='SMS Bomber был скачен в папку C:\Bomber')
         return "exit"
 
-file = Button(text='Скачать HZF Bomber V1.2', command=download)
+def downloadEmail():
+    mkdir("c//Bomber")
+    f=open(r'c/Bomber/HZF Email Bomber.zip', "wb")
+    ufr = requests.get("https://github.com/AvenCores/HZF-Email-Bomber/releases/download/V1.0/Email.Bomber.by.HZF.zip")
+    f.write(ufr.content)
+    f.close()
+    messagebox.showinfo(title="Удачно", message='Email Bomber был скачен в папку C:\Bomber')
+    return "exit"
+
+
+file = Button(text='Скачать HZF Bomber V1.2', command=downloadSms)
 file.pack()
-file.place(x=100, y=30)
+file.place(x=11, y=26)
+file = Button(text='Скачать HZF Email Bomber V1.2', command=downloadEmail)
+file.pack()
+file.place(x=11, y=63)
 poetry = 'Downloader manager by HZF'
 label3 = Label(text=poetry, justify=CENTER)
-label3.place(x=10, y=90)
+label3.place(x=10, y=110)
 root.mainloop()
