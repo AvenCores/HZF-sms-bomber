a20c114804a60a50962aa634e55d68217e18f8a1
AvenCores <64781822+AvenCores@users.noreply.github.com>
2021-03-14 16:52:16 +0300

    Update Install.py

--- Install.py
+++ Install.py
@@ -6,4 +6,5 @@ bib = ["tk", "colorama", "bs4", "as", "termcolor", "Label", "colored", "requests
 for i in range(len(bib)):
     print("Установка "+bib[i]+"...\033[0m")
     os.system("pip3 install "+bib[i])
+os.system('cls')
 print("Установка завершена!")
